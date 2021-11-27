a5ec01b1893f7799741725b022b177d16e811286
Spinier <artsh94@gmail.com>
2021-11-27 11:45:54 +0200

    Created routes and login apge

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact={true}> <Dashboard></Dashboard> </Route>
+        <Route path='/login' exact={true}> <Login></Login> </Route>
+        <Route path='*' exact={true}> <Error></Error> </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
